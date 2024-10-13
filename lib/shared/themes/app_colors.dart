2b6f5fab5ce213d648a0c5e5decc07bc5eda9b13
joutace <ojoutace@gmail.com>
2024-10-13 18:50:34 -0300

    chore (styles): add default colors variables and texts styles

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFD30025);
+  static const gradient = Color(0xAC8B011A);
+  //background Colors
+  static const background = Color(0xFFF9F9F9);
+  static const greyBackground2 = Color(0xFFF3F5F7);
+  static const greyBackground = Color(0xFFF9F9F9);
+  static const white = Color(0xFF000000);
+  static const black = Color(0xFFFFFFFF);
+  static const white40 = Color(0xFF999999);
+  //Text Colors
+  static const darkBlue = Color(0xFF363E48);
+  static Color header = const Color(0xFF040D1B).withOpacity(0.8);
+  static const blackBlue = Color(0xFF363E48);
+  static const greyBlue = Color(0xFFACB5BF);
+  static const blueGrey = Color(0xFF8296AB);
+  static const greyBlueText = Color(0xFF81868D);
+  static const darkBlue2 = Color(0xFF4F565F);
+}
---
+++ lib/shared/themes/app_text_styles.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  static final TextStyle h1 = GoogleFonts.notoSans(
+    fontWeight: FontWeight.w300,
+    fontSize: 96,
+  );
+  static final TextStyle h2 = GoogleFonts.notoSans(
+    fontWeight: FontWeight.w300,
+    fontSize: 60,
+  );
+  static final TextStyle h3 = GoogleFonts.notoSans(
+    fontWeight: FontWeight.normal,
+    fontSize: 48,
+  );
+  static final TextStyle h4 = GoogleFonts.notoSans(
+    fontWeight: FontWeight.normal,
+    fontSize: 34,
+  );
+  static final TextStyle h5 = GoogleFonts.notoSans(
+    fontWeight: FontWeight.normal,
+    fontSize: 24,
+  );
+  static final TextStyle h6 = GoogleFonts.notoSans(
+    fontWeight: FontWeight.w500,
+    fontSize: 20,
+  );
+  static final TextStyle subtitle1 = GoogleFonts.notoSans(
+    fontWeight: FontWeight.normal,
+    fontSize: 16,
+  );
+  static final TextStyle subtitle2 = GoogleFonts.notoSans(
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+  static final TextStyle body1 = GoogleFonts.notoSans(
+    fontWeight: FontWeight.normal,
+    fontSize: 16,
+  );
+  static final TextStyle body2 = GoogleFonts.notoSans(
+    fontWeight: FontWeight.normal,
+    fontSize: 14,
+  );
+  static final TextStyle button = GoogleFonts.notoSans(
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+  static final TextStyle caption = GoogleFonts.notoSans(
+    fontWeight: FontWeight.normal,
+    fontSize: 12,
+  );
+  static final TextStyle overline = GoogleFonts.notoSans(
+    fontWeight: FontWeight.normal,
+    fontSize: 10,
+  );
+}
