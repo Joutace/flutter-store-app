6c5a7d548ce30633d5ea975725c73c7316912158
joutace <ojoutace@gmail.com>
2024-10-14 09:50:19 -0300

    feat (assets and themes): update values

---
+++ lib/shared/resources/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const root = "assets/images";
+  static const splash = "$root/splash.png";
+  static const backgroundLogin = "$root/backgroundLogin.png";
+  static const loginLogo = "$root/loginLogo.png";
+}
--- lib/shared/themes/app_colors.dart
+++ lib/shared/themes/app_colors.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xFFD30025);
-  static const gradient = Color(0xAC8B011A);
-  //background Colors
-  static const background = Color(0xFFF9F9F9);
-  static const greyBackground2 = Color(0xFFF3F5F7);
-  static const greyBackground = Color(0xFFF9F9F9);
-  static const white = Color(0xFF000000);
-  static const black = Color(0xFFFFFFFF);
+  static const primary = Color(0xFFf5eb64);
+  static const gradient = Color(0xFF2e2e2e);
+
+  // Background Colors
+  static const background = Color(0xFFe0e0e0);
+  static const greyBackground = Color(0xFFbdbdbd);
+  static const greyBackgroundAlt = Color(0xFFf7f5e0);
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFFFFFFF);
   static const white40 = Color(0xFF999999);
-  //Text Colors
-  static const darkBlue = Color(0xFF363E48);
-  static Color header = const Color(0xFF040D1B).withOpacity(0.8);
-  static const blackBlue = Color(0xFF363E48);
-  static const greyBlue = Color(0xFFACB5BF);
-  static const blueGrey = Color(0xFF8296AB);
+
+  // Text Colors
+  static const darkGrey = Color(0xFF2e2e2e);
+  static const mediumGrey = Color(0xFF6e6e6e);
+  static const lightGrey = Color(0xFFbdbdbd);
   static const greyBlueText = Color(0xFF81868D);
-  static const darkBlue2 = Color(0xFF4F565F);
+
+  static Color header = const Color(0xFF2e2e2e).withOpacity(0.8);
 }
