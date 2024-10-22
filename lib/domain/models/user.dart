bde7459a41b34433e16d743aee06893987aff992
joutace <ojoutace@gmail.com>
2024-10-21 23:06:52 -0300

    feat (user-model): update user model data

--- lib/domain/models/user.dart
+++ lib/domain/models/user.dart
@@ -3,18 +3,26 @@ part 'user.g.dart';
 
 @JsonSerializable()
 class User {
-  final String email;
+  String? email;
   final double id;
+  String? avatar;
+  final String name;
   final String first_name;
   final String last_name;
-  String? avatar;
+  final String year;
+  final String color;
+  final String pantone_value;
 
   User(
     this.email,
     this.id,
+    this.avatar,
+    this.name,
+    this.year,
+    this.color,
+    this.pantone_value,
     this.first_name,
     this.last_name,
-    this.avatar,
   );
 
   factory User.fromJson(Map<String, dynamic> data) => _$UserFromJson(data);
--- lib/domain/models/user.g.dart
+++ lib/domain/models/user.g.dart
@@ -7,17 +7,25 @@ part of 'user.dart';
 // **************************************************************************
 
 User _$UserFromJson(Map<String, dynamic> json) => User(
-      json['email'] as String,
+      json['email'] as String?,
       (json['id'] as num).toDouble(),
+      json['avatar'] as String?,
+      json['name'] as String,
+      json['year'] as String,
+      json['color'] as String,
+      json['pantone_value'] as String,
       json['first_name'] as String,
       json['last_name'] as String,
-      json['avatar'] as String?,
     );
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'email': instance.email,
       'id': instance.id,
+      'avatar': instance.avatar,
+      'name': instance.name,
       'first_name': instance.first_name,
       'last_name': instance.last_name,
-      'avatar': instance.avatar,
+      'year': instance.year,
+      'color': instance.color,
+      'pantone_value': instance.pantone_value,
     };
